2a039f56132df0224d817363e410ba25ca5db8f9
student <you@example.com>
2024-11-27 14:39:07 +0100

    27/11 14h39 Hero update

--- Projet/app_hero.py
+++ Projet/app_hero.py
@@ -2,19 +2,23 @@ import time
 import sys
 import tkinter as tk
 
+class Frame_Accueil(tk.Frame):
+    def __init__(self,parent):
+        super().__init__(parent)
+        tk.Label(self, text="PYTHONLAND QUEST", fg="red4", bg="gray65", font=("arial", 100)).pack(pady=100)
+        tk.Button(self, text="Commencer", fg="black", bg="seashell3", font=("arial", 70)).pack(pady=20)
+        tk.Button(self, text="Explication/But du jeu", fg="black", bg="seashell3", font=("arial", 70)).pack(pady=20)
+        tk.Button(self, text="Quitter le jeu", fg="black", bg="seashell3", font=("arial", 70),command=parent.destroy).pack(pady=20)
 
 #Fenetre de jeu
 root = tk.Tk()
 root.title("PYTHONLAND QUEST - Projet de Formation - Margoum Yassine - 2024")
 root.geometry("1920x1080")
-root.resizable(False, False)  # non-resizable width, height
+root.resizable(True, True)  # non-resizable width, height
 
 #Menu principal
-tk.Label(root, text = "PYTHONLANDE QUEST", fg="red4", bg="gray65", font=("arial",100)).pack(pady=100)
-tk.Label(root, text = "Commencer", fg="black", bg="seashell3", font=("arial",70)).pack(pady= 20)
-tk.Label(root, text = "Explication/But du jeu", fg="black", bg="seashell3", font=("arial",70)).pack(pady=20)
-tk.Label(root, text = "Quitter le jeu", fg="black",bg="seashell3", font=("arial",70)).pack(pady=20)
-#tk.Button(frm, text)
+main_window = Frame_Accueil(root)
+main_window.pack(fill='both',expand=True)
 
 #Main loop (blocage ici tant qu'on n'a pas quitté)
 root.mainloop()
