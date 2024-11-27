b6d97a910f7661aeb3d53f0b72cbeabca63581cf
student <you@example.com>
2024-11-27 15:29:07 +0100

    App hero Update 15h29

--- Projet/app_hero.py
+++ Projet/app_hero.py
@@ -4,23 +4,16 @@ import tkinter as tk
 
 #Frame détaillé du menu principale
 class Frame_Accueil(tk.Frame):
-    def __init__(self,parent,controller):
+    def __init__(self,parent):
         super().__init__(parent)
-        self.controller = controller
         tk.Label(self, text="PYTHONLAND QUEST", fg="red4", bg="gray65", font=("arial", 100)).pack(pady=100)
         tk.Button(self, text="Commencer", fg="black", bg="seashell3", font=("arial", 70)).pack(pady=20)
-        tk.Button(self, text="Explication/But du jeu", fg="black", bg="seashell3", font=("arial", 50),
-                  command=lambda: controller.show_frame("But_Jeu")).pack(pady=20)
+        tk.Button(self, text="Explication/But du jeu", fg="black", bg="seashell3", font=("arial", 50)).pack(pady=20)
         tk.Button(self, text="Quitter le jeu", fg="black", bg="seashell3", font=("arial", 50),command=parent.destroy).pack(pady=20)
 
 class But_Jeu(tk.Frame):
-    def __init__(self,parent,controller):
+    def __init__(self,parent):
         super().__init__(parent)
-        self.controller = controller
-        tk.Label(self, text="Explications du Jeu", fg="black", bg="white", font=("arial", 80)).pack(pady=100)
-        tk.Button(self, text="Retour", fg="black", bg="seashell3", font=("arial", 50),
-                  command=lambda: controller.show_frame("Frame_Accueil")).pack(pady=50)
-
 
 #Fenetre de jeu
 root = tk.Tk()
