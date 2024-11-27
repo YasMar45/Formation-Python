47d6fa6820a0437c49d35b03100d3ce26feef1eb
student <you@example.com>
2024-11-27 13:33:15 +0100

    27/11 13h33 Hero update

--- Projet/Projet_Hero.py
+++ Projet/Projet_Hero.py
@@ -1,29 +1,5 @@
 import time
 import sys
-import tkinter as tk
-
-
-#Fenetre de jeu
-root = tk.Tk()
-root.title("PYTHONLAND QUEST - Projet de Formation - Margoum Yassine - 2024")
-root.geometry("1920x1080")
-root.resizable(False, False)  # non-resizable width, height
-
-#Menu principal
-tk.Label(root, text = "PYTHONLANDE QUEST", fg="red4", bg="gray65", font=("arial",100)).pack(pady=100)
-tk.Label(root, text = "Commencer", fg="black", bg="seashell3", font=("arial",70)).pack(pady= 20)
-tk.Label(root, text = "Explication/But du jeu", fg="black", bg="seashell3", font=("arial",70)).pack(pady=20)
-tk.Label(root, text = "Quitter le jeu", fg="black",bg="seashell3", font=("arial",70)).pack(pady=20)
-tk.Button(frm, text)
-def start_button(self):
-    pass
-
-
-
-
-#Main loop (blocage ici tant qu'on n'a pas quitté)
-root.mainloop()
-
 
 #Fonction pour les textes qui defilent lentemenet
 def slow_print(text, delay=0):
