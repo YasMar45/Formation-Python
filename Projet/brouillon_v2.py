d468641bbfd4fa3faa63ab431a928505bce7a84c
student <you@example.com>
2024-11-29 10:58:28 +0100

    App Hero Start Over 29/11 10h58

--- Projet/brouillon_v2.py
+++ Projet/brouillon_v2.py
@@ -6,3 +6,16 @@ class Game(tk.Frame):
         super().__init__(parent)
         self.parent = parent
 
+# Fenetre de jeu
+root = tk.Tk()
+root.title("PYTHONLAND QUEST - Projet de Formation - Margoum Yassine - 2024")
+root.geometry("1920x1080")
+root.resizable(True, True)  # non-resizable width, height
+
+# Menu principal
+main_window = Game(root)
+main_window.pack(fill='both', expand=True)
+
+# Main loop (blocage ici tant qu'on n'a pas quittée)
+root.mainloop()
+
