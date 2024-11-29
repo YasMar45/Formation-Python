8bcfd5d2144ee618ff4fd20d42586607af94d282
student <you@example.com>
2024-11-29 12:31:00 +0100

    App Hero Start Over 29/11 12h17

--- Projet/brouillon_v2.py
+++ Projet/brouillon_v2.py
@@ -11,16 +11,20 @@ class Game(tk.Frame):
         self.parent.title("PYTHONLAND QUEST - Projet de Formation - Margoum Yassine - 2024")
 
         # Interface de base
-        self.int = tk.Canvas(self.parent, width=largeur_fenetre, height=hauteur_fenetre, bg='black')
+        self.int = tk.Frame(self.parent, width=largeur_fenetre, height=hauteur_fenetre, bg='black')
         self.int.pack()
 
         # Rectangle pour le texte
         self.frm_box = tk.Frame(self.parent, bg='white', width= largeur_fenetre, height= hauteur_fenetre, relief= tk.SUNKEN)
         self.frm_box.place(x=0, y=700)
 
-        # Button continuer
-        self.btn_continue = tk.Button(self.parent, text="Continuer", fg="black",bg="seashell3", font=("arial", 20))
-        self.btn_continue.place(x=1700, y=650)
+        # Endroit où le texte apparait
+        self.lb_text = tk.Label(text="Test", fg="black", bg="white", font=("arial", 20))
+        self.lb_text.place(x=100, y=750)
+
+        # Button "Continuer"
+        self.btn_continue = tk.Button(self.parent, text="Continuer", fg="black",bg="seashell3", font=("arial", 30))
+        self.btn_continue.place(x=1650, y=620)
 
 
 # Lancement du jeu
