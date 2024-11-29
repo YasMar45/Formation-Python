6c794ba901562536ddafc836e8c7e740087f5ae4
student <you@example.com>
2024-11-29 12:00:13 +0100

    App Hero Start Over 29/11 12h00

--- Projet/brouillon_v2.py
+++ Projet/brouillon_v2.py
@@ -8,18 +8,11 @@ hauteur_fenetre = 1080
 class Game(tk.Frame):
     def __init__(self, parent):
         self.parent = parent
-
-        #Titre de l'interface
         self.parent.title("PYTHONLAND QUEST - Projet de Formation - Margoum Yassine - 2024")
 
-        #Interface de base
+        # Interface de base
         self.int = tk.Canvas(self.parent, width=largeur_fenetre, height=hauteur_fenetre, bg='black')
         self.int.pack()
-
-        self.frm_box = tk.Frame(self.parent, height=25, bg='white')
-
-
-
 
 
 # Lancement du jeu
