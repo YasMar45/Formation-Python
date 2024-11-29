f372447db241d093f0e9530e4a84637accfda99c
student <you@example.com>
2024-11-29 12:17:22 +0100

    App Hero Start Over 29/11 12h17

--- Projet/brouillon_v2.py
+++ Projet/brouillon_v2.py
@@ -14,6 +14,14 @@ class Game(tk.Frame):
         self.int = tk.Canvas(self.parent, width=largeur_fenetre, height=hauteur_fenetre, bg='black')
         self.int.pack()
 
+        # Rectangle pour le texte
+        self.frm_box = tk.Frame(self.parent, bg='white', width= largeur_fenetre, height= hauteur_fenetre, relief= tk.SUNKEN)
+        self.frm_box.place(x=0, y=700)
+
+        # Button continuer
+        self.btn_continue = tk.Button(self.parent, text="Continuer", fg="black",bg="seashell3", font=("arial", 20))
+        self.btn_continue.place(x=1700, y=650)
+
 
 # Lancement du jeu
 root = tk.Tk()
